b11d0f5304b989b80f3e4089e46186896e295d0b
ckhacks34 <150327378+ckhacks34@users.noreply.github.com>
2025-03-12 20:40:43 +0200

    commit 8

--- contact.php
+++ contact.php
@@ -15,8 +15,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Username = "ngongochris415@gmail.com";
-        $mail->Password = "vbn.3.4.5";
+        $mail->Username = "chrismoses2b2@gmail.com";
+        $mail->Password = "fnul kieb veka cdlx";
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
